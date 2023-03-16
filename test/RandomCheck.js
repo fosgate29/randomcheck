0185fb5ef36459c7d4fa2d98f27188ba120bfd52
Luiz Soares <luizhamilton29@gmail.com>
2023-03-16 19:58:26 -0300

    Fixed deploy script and added more test

--- test/RandomCheck.js
+++ test/RandomCheck.js
@@ -6,7 +6,7 @@ describe('RandomCheck', function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
+  async function deployRandomFixture() {
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
     const uri = 'http://sth';
@@ -19,10 +19,16 @@ describe('RandomCheck', function () {
 
   describe('Deployment', function () {
     it('Should mint 10 tokens of id 1', async function () {
-      const { randomCheck, owner } = await loadFixture(deployOneYearLockFixture);
+      const { randomCheck, owner } = await loadFixture(deployRandomFixture);
 
       await randomCheck.mint();
       expect(await randomCheck.balanceOf(owner.address, 1)).to.equal(10);
+    });
+
+    it('Should random mint', async function () {
+      const { randomCheck } = await loadFixture(deployRandomFixture);
+
+      await randomCheck.randomMint();
     });
   });
 });
